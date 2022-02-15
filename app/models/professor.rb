77aba864a6c8a1d340c7730d5500fbb827d98e22
Maximilian Wagner <maw3350@gmail.com>
2022-02-15 18:34:08 -0500

    feat: completed user story 4, added model testing for user story 3

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -6,4 +6,8 @@ class Professor <ApplicationRecord
 
   has_many :professor_students
   has_many :students, through: :professor_students
+
+  def avg_student_age
+    students.average(:age).to_f
+  end
 end
--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -16,4 +16,16 @@ RSpec.describe 'professor show page' do
     expect(page).to_not have_content(malfoy.name)
   end
 
+  it 'shows average student age' do
+
+    snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+    longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+    ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+    ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+
+    visit "/professors/#{snape.id}"
+    expect(page).to have_content("Average student age:11")
+  end
+
 end
